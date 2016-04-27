1bca971b50e1d652f72f4697dd4bacb0f86da3aa
Nick <nimamshah@ufl.edu>
2016-04-27 03:34:36 -0400

    Add final document

--- Assignment4/test.sql
+++ Assignment4/test.sql
@@ -1,8 +1,7 @@
-EXECUTE Domination.GetThem ('Brad', .3);
-EXECUTE Domination.GetThem ('Brad',.95);
-EXECUTE Domination.GetThem ('Donna', .95);
-EXECUTE Domination.GetThem ('Sandra',.95);
-EXECUTE Domination.GetThem ('Jennifer', .95);
-EXECUTE Domination.GetThem ('Michael', .99);
-EXECUTE Domination.GetThem ('Sandra',.5);
-EXECUTE Domination.GetThem ('Jennifer', .5);
+SELECT *
+FROM sightings
+WHERE name = 'Draperia';
+
+SELECT *
+FROM flowers
+WHERE genus = 'Draperia';
